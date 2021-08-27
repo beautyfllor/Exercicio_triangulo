14cd39f7968f49f5d83d5ba01e0bdefe91eaa41d
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2021-08-27 15:50:19 -0300

    correcao de or para and

--- src/triangulo/Triangulo.java
+++ src/triangulo/Triangulo.java
@@ -18,7 +18,7 @@ public class Triangulo {
 		lado3 = leitor.nextDouble();
 		leitor.close();
 		
-		if(lado1 + lado2 < lado3 || lado2 + lado3 < lado1 || lado1 + lado3 < lado2) {
+		if(lado1 + lado2 < lado3 && lado2 + lado3 < lado1 && lado1 + lado3 < lado2) {
 			System.out.println("Os valores digitados não resultam em um triângulo!");
 		} else if(lado1 == lado2 && lado1 == lado3 && lado2 == lado3) {
 			System.out.println("Este triângulo é Equilátero!");
